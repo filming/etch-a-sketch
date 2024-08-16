80f4edc052172b52dc7edbd5afac3b917294fdde
Filming <79931565+filming@users.noreply.github.com>
2024-08-16 13:03:50 -0400

    style: Make 16x16 grid visible

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ const gridContainer = document.querySelector(".grid");
 
 for (let i = 0; i < 16; i++){
     const newRowDiv = document.createElement("div");
+    newRowDiv.classList.add("grid__row");
     
     for (let j = 0; j < 16; j++){
         const newColDiv = document.createElement("div");
+        newColDiv.classList.add("grid__col");
+
         newRowDiv.appendChild(newColDiv);
     }
 
