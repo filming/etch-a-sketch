55ceb450fc2cf536d27f71e7cf64278de1ebd9b4
Filming <79931565+filming@users.noreply.github.com>
2024-08-16 16:24:48 -0400

    refactor: Make grid creation process modular

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,16 +1,24 @@
-// create a 16x16 grid of divs
+// create a grid of divs
 const gridContainer = document.querySelector(".grid");
 
-for (let i = 0; i < 16; i++){
-    const newRowDiv = document.createElement("div");
-    newRowDiv.classList.add("grid__row");
-    
-    for (let j = 0; j < 16; j++){
-        const newColDiv = document.createElement("div");
-        newColDiv.classList.add("grid__col");
+function createGrid(rows, cols){
+    for (let i = 0; i < rows; i++){
+        const newRowDiv = document.createElement("div");
+        newRowDiv.classList.add("grid__row");
+        
+        for (let j = 0; j < cols; j++){
+            const newColDiv = document.createElement("div");
+            newColDiv.classList.add("grid__col");
 
-        newRowDiv.appendChild(newColDiv);
+            newRowDiv.appendChild(newColDiv);
+        }
+
+        gridContainer.appendChild(newRowDiv);
     }
-
-    gridContainer.appendChild(newRowDiv);
 }
+
+createGrid(16, 16);
+
+gridContainer.addEventListener("mousemove", (e) => {
+    e.target.style["background-color"] = "#ff950079";
+});
